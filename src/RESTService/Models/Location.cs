7004714c41a8dd3ffe7b43a43b5badc692d35469
Hooman Yar <hyar@evolution1.com>
2016-10-20 16:02:04 -0500

    init commit - basic location model

---
+++ src/RESTService/Models/Location.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RESTService.Models
+{
+    public class Location
+    {
+        public string Key { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ src/RESTService/Models/LocationRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace RESTService.Models
+{
+    public class LocationRepository : ILocationRepository
+    {
+        /// <summary>
+        /// mapping a location to a string key
+        /// using a thread-safe collection
+        /// todo: add linq queries from db
+        /// </summary>
+        private static ConcurrentDictionary<string, Location> _locations = new ConcurrentDictionary<string, Location>();
+
+        public void Add(Location location)
+        {
+            location.Key = Guid.NewGuid().ToString();
+            _locations.TryAdd(location.Key, location);
+        }
+
+        public IEnumerable<Location> GetAll()
+        {
+            return _locations.Values;
+        }
+
+        public void Update(Location locatoin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Location Find(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
