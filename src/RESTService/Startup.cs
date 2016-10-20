3f73037e6efc2ab4418e493f448641d805bae2c5
Hooman Yar <hyar@evolution1.com>
2016-10-20 16:24:06 -0500

    repo model dependency injection

--- src/RESTService/Startup.cs
+++ src/RESTService/Startup.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RESTService.Models;
 
 namespace RESTService
 {
@@ -38,6 +35,9 @@ namespace RESTService
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddMvc();
+
+            // inject repo model into our controller
+            services.AddSingleton<ILocationRepository, LocationRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
