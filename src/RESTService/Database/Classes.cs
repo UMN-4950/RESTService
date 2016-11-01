1d6918206be016dc9c954a7173e3ad39f83f4a92
Jakob Johnson <jakob.johnson8@gmail.com>
2016-11-01 10:35:32 -0500

    Base classes and context for database

---
+++ src/RESTService/Database/Classes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTService.Database
+{
+    public class Location
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitute { get; set; }
+        public DateTime RecordedTime { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public List<Location> Locations { get; set; }
+        public List<User> Friends { get; set; }
+    }
+
+    public class Event
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public Location Location { get; set; }
+        public List<User> Attendees { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ src/RESTService/Database/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTService.Database
+{
+    public class Context:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
