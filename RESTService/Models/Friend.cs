0a3063409d17aea0c3a5fb927518d605f337ad4c
Hooman Yar <hooman.yar@gmail.com>
2017-03-03 16:39:13 -0600

    status in friends model

--- RESTService/Models/Friend.cs
+++ RESTService/Models/Friend.cs
@@ -5,6 +5,7 @@ namespace RESTService.Models
     public class Friend
     {
         public int Id { get; set; }
+        public string Status { get; set; }
 
         // ForeignKey
         public int UserId { get; set; }
